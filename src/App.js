efb33827153e550cb69a5264bd4491ace0c2e849
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-05 06:56:48 -0400

    Add ui container styling to the main app.js div

--- src/App.js
+++ src/App.js
@@ -5,20 +5,22 @@ import StreamEdit from './components/streams/StreamEdit';
 import StreamDelete from './components/streams/StreamDelete';
 import StreamList from './components/streams/StreamList';
 import StreamShow from './components/streams/StreamShow';
+import Header from './components/Header';
 
 
 const App = () => {
   return (
-    <div>
-      <BrowserRouter>
-        <div>
-          <Route path="/" exact component={StreamList} />
-          <Route path="/streams/new" exact component={StreamCreate} />
-          <Route path="/streams/edit" exact component={StreamEdit} />
-          <Route path="/streams/delete" exact component={StreamDelete} />
-          <Route path="streams/show" exact component={StreamShow} />
-        </div>
-      </BrowserRouter>
+    <div className="ui container">
+      <Header />
+        <BrowserRouter>
+          <div>
+            <Route path="/" exact component={StreamList} />
+            <Route path="/streams/new" exact component={StreamCreate} />
+            <Route path="/streams/edit" exact component={StreamEdit} />
+            <Route path="/streams/delete" exact component={StreamDelete} />
+            <Route path="streams/show" exact component={StreamShow} />
+          </div>
+        </BrowserRouter>
     </div>
   );
 }
