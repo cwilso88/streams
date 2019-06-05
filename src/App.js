ec5668450088cf9d92b4f7efff32077986c9b2b8
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-05 06:33:46 -0400

    Imported each stream component in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
 
 
-function App() {
+const App = () => {
   return (
     <div>
       <BrowserRouter>
