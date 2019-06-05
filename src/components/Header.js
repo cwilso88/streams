25be37235a2cfa6b42c6a6af90d5765d3a7eed7f
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-05 07:00:16 -0400

    Set up links for header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="ui secondary pointing menu">
+            <div className="right menu">
+                <Link to="/" className="item">
+                    Streamer
+                </Link>
+            </div>
+            <div className="right menu">
+                <Link to="/" className="item">
+                    All Stream
+                </Link>
+            </div>
         </div>
     )
 }
