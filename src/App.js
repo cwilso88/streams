b495039339f4bcaf9a5f9381d7d127c20ce86e4d
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-05 06:38:47 -0400

    Set up each component Route in the App.js file

--- src/App.js
+++ src/App.js
@@ -11,8 +11,12 @@ const App = () => {
   return (
     <div>
       <BrowserRouter>
-        <div className="App">
-          <h1>Hello World</h1>
+        <div>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
