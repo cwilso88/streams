d3591a4e2f39c3c9e0401f1e9899a50afe86233f
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-07 07:44:52 -0400

    Added in client id

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class GoogleAuth extends Component {
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '137584578036-g8bptm6j005j1e5kpi0tuho6vu1vrq15.apps.googleusercontent.com'
+            })
+        });
+    }
     render() {
         return (
             <div>
