a836e4d6e4d1f7d7db4afd8ca2dd8e6a008ad86a
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-10 13:32:12 -0400

    added in the renderInput method and passed it in to the component props in each field

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
 class StreamCreate extends Component {
+    renderInput() {
+        return (
+            <input />
+        )
+    }
     render() {
         return (
             <form>
-                <Field name="title"/>
-                <Field name="description"/>
+                <Field name="title" component={this.renderInput} />
+                <Field name="description" component={this.renderInput}/>
             </form>
         )
     }
