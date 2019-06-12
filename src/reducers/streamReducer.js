c1f57b2a7f2c8943440863738ea4faaba6eeaafd
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-12 15:42:56 -0400

    Created reducers for CREATE_STREAM,EDIT_STREAM

--- src/reducers/streamReducer.js
+++ src/reducers/streamReducer.js
@@ -10,7 +10,12 @@ import {
 export default (state = {}, action) => {
     switch (action.type) {
         case FETCH_STREAM:
-            return { ...state, [action.payload.id]: action.payload }
+            return { ...state, [action.payload.id]: action.payload };
+        case CREATE_STREAM:
+            return { ...state, [action.payload.id]: action.payload };
+        case EDIT_STREAM: 
+             return { ...state, [action.payload.id]: action.payload };
+        
         default:
             state;
     }
