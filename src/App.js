9823f8da876f4087ea67a041ac0fb2a67bdad1de
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-17 15:08:34 -0400

    Updated the history path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import StreamDelete from './components/streams/StreamDelete';
 import StreamList from './components/streams/StreamList';
 import StreamShow from './components/streams/StreamShow';
 import Header from './components/Header';
-import history from '../history';
+import history from './history';
 
 
 const App = () => {
