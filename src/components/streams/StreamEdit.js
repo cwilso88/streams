1c12a25ffc1db2dac217bdeba5ecf268b4fcba01
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-17 16:10:13 -0400

    Imported connect into StreamEdit and and created a mapStateToProps variable

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+const StreamEdit = (props) => {
     return (
         <div>
             StreamEdit
@@ -8,4 +9,10 @@ const StreamEdit = () => {
     )
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return { stream:  };
+};
+
+export default connect(
+    mapStateToProps
+)(StreamEdit);
