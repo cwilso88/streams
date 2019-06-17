2952b9bf9f824c8b568e46ea3381135906ea9256
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-17 14:07:36 -0400

    Created the edit and delete buttons

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -10,7 +10,16 @@ class StreamList extends React.Component {
 
     renderAdmin(stream) {
         if (stream.userId === this.props.currentUserId){
-            return <div>Edit/delete</div>
+            return (
+                <div className="right floated content">
+                    <button className="ui button primary">
+                        Edit
+                    </button>
+                    <button className="ui button negative">
+                        Delete
+                    </button>
+                </div>
+            )
         }
     }
 
