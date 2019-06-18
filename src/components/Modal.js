4ce1e081b08d583ec908ef5457c7e97c7c267182
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-18 15:34:45 -0400

    Set up for the modal.js file

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Modal = props => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Modal
