bd1658c123416a15750e0f8e372a4421120ec6fa
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-18 16:38:58 -0400

    Refactored the StreamDelete component

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -2,26 +2,33 @@ import React from 'react';
 import Modal from '../Modal';
 import history from '../../history';
 
-const StreamDelete = () => {
-    const actions = (
-        <React.Fragment>
-            <button className="ui button">Cancel</button>
-            <button className="ui button negative">Delete</button>
-        </React.Fragment>
-    );
+class StreamDelete extends React.Component {
+    componentDidMount() {
+        
+    } 
+    
+    render() {
+        const actions = (
+            <React.Fragment>
+                <button className="ui button">Cancel</button>
+                <button className="ui button negative">Delete</button>
+            </React.Fragment>
+        );
 
-    return (
-        <div>
-            StreamDelete
-            <Modal 
-                header="Delete Stream"
-                content="Are you sure you want to delete this stream?"
-                actions={actions}
-                onDismiss={() => history.push('/')}
-                stopPropogation={(e) => e.stopPropagation()}
-            />
-        </div>
-    );
+
+        return (
+            <div>
+                StreamDelete
+                <Modal 
+                    header="Delete Stream"
+                    content="Are you sure you want to delete this stream?"
+                    actions={actions}
+                    onDismiss={() => history.push('/')}
+                    stopPropogation={(e) => e.stopPropagation()}
+                />
+            </div>
+        );
+    };
 };
 
 export default StreamDelete;
