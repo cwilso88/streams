aaaf8fc8e7126d0da34b370483d5dcf9612937a5
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-18 13:49:06 -0400

    Deleted the connect and createStream imports

--- src/components/streams/StreamForm.js
+++ src/components/streams/StreamForm.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { connect } from 'react-redux';
-import { createStream } from '../../actions';
 
 class StreamForm extends Component {
     renderError = ({ error, touched }) => {
