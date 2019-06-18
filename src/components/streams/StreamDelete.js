ef4ea4e858d50b982de4423bb89b6fcec74d3a2b
Chasity DeLaCruz <chasity.wilson4@gmail.com>
2019-06-18 16:41:03 -0400

    More refactoring

--- src/components/streams/StreamDelete.js
+++ src/components/streams/StreamDelete.js
@@ -4,25 +4,26 @@ import history from '../../history';
 
 class StreamDelete extends React.Component {
     componentDidMount() {
-        
+
     } 
-    
-    render() {
-        const actions = (
+
+    renderActions() {
+        return (
             <React.Fragment>
                 <button className="ui button">Cancel</button>
                 <button className="ui button negative">Delete</button>
             </React.Fragment>
         );
-
-
+    }
+    
+    render() {
         return (
             <div>
                 StreamDelete
                 <Modal 
                     header="Delete Stream"
                     content="Are you sure you want to delete this stream?"
-                    actions={actions}
+                    actions={this.renderActions()}
                     onDismiss={() => history.push('/')}
                     stopPropogation={(e) => e.stopPropagation()}
                 />
